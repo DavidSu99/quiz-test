91995b40a9a28ee146db312bf97f000576c8e251
DavidSu99 <davidsusic99@gmail.com>
2023-08-15 14:41:19 +0200

    added modular structure

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ fetch("./quiz.json")
   .then((quiz) => quiz.json())
   .then((data) => {
     const quiz = data.quiz;
-
+    
     //make the form using the json file
     Object.keys(quiz).forEach((questionKey) => {
       const question = quiz[questionKey];
@@ -19,14 +19,13 @@ fetch("./quiz.json")
       question.options.forEach((option) => {
         const radio = document.createElement("input");
         const divWrapper = document.createElement("div");
+        const label = document.createElement("label");
+
         radio.type = "radio";
         radio.name = questionKey;
         radio.value = option;
-
-        const label = document.createElement("label");
         label.textContent = option;
 
-        //append the elemets to html
         radio.append(label);
         divWrapper.appendChild(radio);
         divWrapper.appendChild(label);
@@ -36,60 +35,68 @@ fetch("./quiz.json")
         fieldset.setAttribute("id", questionKey);
         fieldset.appendChild(document.createElement("br"));
 
-        //check radio with the LS value
-        const storedValue = localStorage.getItem(questionKey);
-        if (storedValue === option) {
-          radio.checked = true;
-        }
-        //check color with LS
-        const colorKey = `${questionKey}-${option}`;
-        const storedColor = localStorage.getItem(colorKey);
-
-        if (storedColor === "correct") {
-          divWrapper.classList.add("correct");
-        } else if (storedColor === "incorrect") {
-          divWrapper.classList.add("incorrect");
-        }
+        //if user submited the form before, show the their anwsers
+        retainedAnwsers(questionKey, option, divWrapper, radio);
       });
       quizForm.append(fieldset);
     });
-
-    quizForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      // get the form data and add it to LS
-      const formData = new FormData(e.target);
-      const formEntries = [...formData.entries()];
-      formEntries.forEach((entry) => {
-        const [key, value] = entry;
-        localStorage.setItem(key, value);
-
-        //check if anwser is correct
-        const question = quiz[key];
-        const fieldset = document.querySelector(`#${key}`);
-
-        question.options.forEach((option) => {
-          const divWrapper = fieldset.querySelector(
-            `#${option.replace(/\s/g, "")}`
-          );
-          //removes colors and update the colors in storage after retrying
-          divWrapper.classList.remove("correct", "incorrect");
-          localStorage.removeItem(`${key}-${option}`);
-
-          if (question.answer.includes(value)) {
-            if (option === value) {
-              divWrapper.classList.add("correct");
-              localStorage.setItem(`${key}-${option}`, "correct");
-            }
-          } else {
-            if (option === value) {
-              divWrapper.classList.add("incorrect");
-              localStorage.setItem(`${key}-${option}`, "incorrect");
-            }
-          }
-        });
-      });
-    });
+    //update the LS if event on quiz
+    quizListener(quiz);
   })
   .catch((error) => {
     console.error("Fetch error:", error);
   });
+
+function retainedAnwsers(questionKey, option, divWrapper, radio) {
+  const storedValue = localStorage.getItem(questionKey);
+  if (storedValue === option) {
+    radio.checked = true;
+  }
+
+  const colorKey = `${questionKey}-${option}`;
+  const storedColor = localStorage.getItem(colorKey);
+
+  if (storedColor === "correct") {
+    divWrapper.classList.add("correct");
+  } else if (storedColor === "incorrect") {
+    divWrapper.classList.add("incorrect");
+  }
+}
+function quizListener(quiz) {
+  quizForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const formEntries = [...formData.entries()];
+    formEntries.forEach((entry) => {
+      const [key, value] = entry;
+      localStorage.setItem(key, value);
+
+      evalAndDisplayAnswers(key, value, quiz);
+    });
+  });
+}
+function evalAndDisplayAnswers(key, value, quiz) {
+  const question = quiz[key];
+  const fieldset = document.querySelector(`#${key}`);
+
+  question.options.forEach((option) => {
+    const divWrapper = fieldset.querySelector(`#${option.replace(/\s/g, "")}`);
+
+    divWrapper.classList.remove("correct", "incorrect");
+    localStorage.removeItem(`${key}-${option}`);
+
+    if (question.answer.includes(value)) {
+      if (option === value) {
+        divWrapper.classList.add("correct");
+        localStorage.setItem(`${key}-${option}`, "correct");
+      }
+    } else {
+      if (option === value) {
+        divWrapper.classList.add("incorrect");
+        localStorage.setItem(`${key}-${option}`, "incorrect");
+      }
+    }
+  });
+}
+578
